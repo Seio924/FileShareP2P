3fe494c29d0364aeb890b3348dd77e77967c563e
Seio924 <134473627+Seio924@users.noreply.github.com>
2023-12-02 17:47:07 +0900

    p2p

--- client.py
+++ client.py
@@ -2,47 +2,33 @@ import socket
 import threading
 
 # 서버 IP 주소와 포트
-server_ip = '서버_IP_주소'
+server_ip = '127.0.0.1'
 server_port = 12345  # 사용할 포트 번호
 
-connecting_client_list = []
 
 def handle_server_connection(client_socket):
     try:
-        # 서버에게 메시지 전송
-        message_to_server = "클라이언트에서 서버로의 메시지입니다."
-        client_socket.sendall(message_to_server.encode())
+        msg = "클라이언트에서 서버로의 메시지입니다."
+        client_socket.send(bytes(msg.encode()))
 
-        # 서버로부터 응답 수신
         response = client_socket.recv(1024)
         print(f"서버로부터의 응답: {response.decode()}")
 
-        # 서버로부터 받은 메시지 반환
         return response.decode()
 
     except Exception as e:
         print(f"통신 에러: {e}")
         return None
-    finally:
-        # 소켓 닫기
-        client_socket.close()
-        print("쓰레드 종료")
 
-def connect_to_server():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+def connect_to_server(client_socket):
     try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # 서버에 연결
         client_socket.connect((server_ip, server_port))
         print(f"서버에 연결되었습니다. 서버 IP: {server_ip}, 포트: {server_port}")
 
-        # 쓰레드 생성 및 시작
-        thread = threading.Thread(target=handle_server_connection, args=(client_socket,))
-        thread.start()
-
-        # 쓰레드 종료 대기 및 서버로부터 받은 메시지 반환
-        thread.join()
-        return thread.result
+        return handle_server_connection(client_socket)
 
     except Exception as e:
         print(f"연결 에러: {e}")
@@ -55,32 +41,45 @@ def connect_to_client(target_ip, target_port):
     return target_socket
 
 def get_file(client_socket):
+    connecting_client_list = select_client(client_socket)
+
+    for client in connecting_client_list:
+        msg = "Give_file" #파일의 어느부분 달라고 하는지 정보 보내야함
+        client.send(bytes(msg.encode()))
+
     while True:
         try:
-            client_socket.sendall(message_to_server.encode())
+            response = client_socket.recv(1024) #어떤 클라이언트에게 뭐가 왔는지 메시지에 있어야함
+            print(response)
 
-            # 서버로부터 응답 수신
+            #connecting_client_list와 비교해서 모든 클라이언트에게 받았으면 종료
+            break
+        except ConnectionResetError:
+            break
+    
+    
+
+def give_file(conn, client_socket):
+    while True:
+        try:
             response = client_socket.recv(1024)
+
+            print(response)
+
+            msg = "here is the file"
+            conn.send(bytes(msg.encode()))
+
 
         except ConnectionResetError:
             break
 
-def give_file(client_socket):
-    while True:
-        try:
-            client_socket.sendall(message_to_server.encode())
 
-            # 서버로부터 응답 수신
-            response = client_socket.recv(1024)
+def select_client(client_socket):
+    connecting_client_list = []
 
-        except ConnectionResetError:
-            break
-
-
-def select_client():
-    result_message = connect_to_server() # (주소)|(주소)
-
-    clients_address = result_message.split("|")
+    result_message = connect_to_server(client_socket) # (주소)|(주소)
+    print(result_message)
+    clients_address = result_message.split("|") # ("127.0.0.1", 34521)
 
     #result_message split해서 받은 모든 클라이언트의 주소 가져옴
 
@@ -88,34 +87,26 @@ def select_client():
         target_ip, target_port= i[1:-1].split(",")
         connecting_client_list.append(connect_to_client(target_ip, int(target_port))) # for문 돌려서 배열에 넣기
 
-    # 메시지 전송을 위한 쓰레드 생성
-    #for문 돌려서 배열 수 만큼 쓰레드 생성
-
-    for i in connecting_client_list:
-        client_thread = threading.Thread(target=handle_clients_connection, args=(i))
-        client_thread.start()
+    return connecting_client_list
 
 
 if __name__ == "__main__":
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # 주소와 포트 바인딩
-    client_socket.bind(('localhost', 8888))
 
-    # 연결 대기
-    client_socket.listen(4)
+    
+    thread1 = threading.Thread(target=get_file, args=(client_socket,))
+    thread1.start()
 
     while True:
         try:
             count = count + 1
             conn, addr = client_socket.accept()  # 해당 소켓을 열고 대기
             
-            thread1 = threading.Thread(target=get_file, args=(conn))
-            thread2 = threading.Thread(target=give_file, args=(conn))
-            thread1.start()
+            thread2 = threading.Thread(target=give_file, args=(conn, client_socket))
+            
             thread2.start()
 
             
         except:
             exit(0)
-    start_client()
--- server.py
+++ server.py
@@ -2,23 +2,25 @@ import socket
 import threading
 
 # 서버 IP 주소와 포트
-server_ip = '서버_IP_주소'
+server_ip = '127.0.0.1'
 server_port = 12345  # 사용할 포트 번호
+g = []
 
 def handle_client(client_socket):
+    global g
     try:
         # 클라이언트로부터 데이터 수신 및 출력
         data = client_socket.recv(1024)
         print(f"수신: {data.decode()}")
 
         # 클라이언트에게 응답 전송
-        response = "서버에서 클라이언트로의 응답입니다."
-        client_socket.sendall(response.encode())
+        response = str(g[-1].getpeername()) + "|"
+        client_socket.send(bytes(response.encode()))
     except Exception as e:
         print(f"에러: {e}")
     finally:
         # 소켓 닫기
-        client_socket.close()
+        print("끝")
 
 # 서버 소켓 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,6 +32,8 @@ print(f"서버가 {server_ip}:{server_port}에서 시작되었습니다.")
 while True:
     # 클라이언트 연결 대기
     client_socket, client_address = server_socket.accept()
+    g.append(client_socket)
+
     print(f"새로운 연결: {client_address}")
 
     # 클라이언트를 처리하는 스레드 시작
