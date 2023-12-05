0a266c9b243d0df038b8db7473a36220f4ddbde8
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2023-12-06 05:34:45 +0900

    청크값 청크리스트에 추가

--- client.py
+++ client.py
@@ -72,17 +72,20 @@ def peer_handler(client_socket, thread_num):
             peer_connecting_sock[-1].send(peer_msg.encode("utf-8")) # 다른 피어랑 소통
 
         for i in range(len(target_client_list)):
+
             # 다른 클라이언트에게 파일 받기
             # 여기도 for문으로 데이터 받고 for문 안에서 파일 업데이트 실시
             peer_data = peer_connecting_sock[i].recv(1024).decode() # 파일 청크
             send_client = peer_data[0] # 청크를 보낸 친구
 
             peer_data = peer_data[1:] # 튜플 모양새 문자열만 추출
-            
+            peer_chunk = peer_data.split(",")[1]
+
             # 여기서가 문제 왜냐면 (1300, djf;lfj;jfdjfkajfdjaf;jdk) 이런식으로 올거야.
-            # 이 문자열을 튜플로 바꿔야하는데....
-            print(peer_data)
-            update_chunks_list[int(send_client)-1].append(peer_data)
+            # 해당 청크리스트의 클라이언트 인덱스에 차례대로 청크값만 저장
+            # 예를 들어, 클라이언트1의 128번째 청크 > update_chunks_list[0][127]
+            
+            update_chunks_list[int(send_client)-1].append(peer_chunk)
 
             time.sleep(0.01)
 
