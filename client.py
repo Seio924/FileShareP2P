824563b31e4953ad4faab924c09686a735c5b75b
mulkong-21 <134470507+mulkong-21@users.noreply.github.com>
2023-12-06 04:11:56 +0900

    파일 청크 전송 틀

--- client.py
+++ client.py
@@ -1,60 +1,108 @@
 import socket
 import threading
+import random
+import time
+import os
 
+# 청크 단위로 파일 쪼개기 (완)
 
-def receive_messages(peer_connection, thread_num):
+#파일을 청크 단위로 나눠 리스트로 만들기 (청크 인덱스, 청크내용)
+def read_file_in_chunks(file_path, chunk_size=256 * 1024):
+    with open(file_path, 'rb') as file:
+        index = 0
+        chunks_list = []
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+            chunks_list.append((index, chunk))
+            index += 1
+        return chunks_list
+    
+
+def receive_messages(peer_connection, thread_num): # 여기서 해당 클라이언트에게 청크 줘
+    global chunks_list
     try:
         data = peer_connection.recv(1024).decode()
+        want_client, want_index_recv = data.split("|") # 내 것의 청크를 원하는 클라이언트, 원하는 청크인덱스
         print(data)
     except Exception as e:
         print(f"Error receiving data: {e}")
 
     # 파일 보내주기
-    msg = thread_num + "번이 파일을 보냈습니다."
+    # 몇번 클라이언트의 청크인지 보내야함. 내가 몇번인지 말고 (수정)
+    print(thread_num + "번이 파일을 보냈습니다.")
+    msg = thread_num + chunks_list[int(want_index_recv)] # 친구가 원하는 청크 보내준다.
     peer_connection.send(msg.encode("utf-8"))
     
     
 
-def peer_handler(client_socket, peer_connecting_sock, thread_num):
-    
+def peer_handler(client_socket, thread_num):
+    global update_chunks_list
     peer_connecting_sock = []
 
     while True:
         #파일 나누고 자신한테 없는 파일들 정보 서버에게 물어보기
-        msg = "Where_is"
-        client_socket.send(msg.encode("utf-8"))
+        #랜덤으로 클라이언트 고른다. 
+        #근데 여기서 내가 나를 고를수 있잖아 그럼 다시 골라야겠지
+        #그리고 순서대로 청크를 달라고 할거기때문에 길이를 가지고 원하는 청크 정한다.
+        while True:
+            random_client = random.randint(1, 4) 
+            if random_client != thread_num or len(update_chunks_list[random_client-1]) != 1954: 
+                # 내 스레드 번호랑 다르다면 멈춰 또는 내가 고른 랜덤 클라이언트로부터 청크를 다 받았다면 다시 뽑아야지
+                break
+        
+        # 나중에 청크 더 받고 싶은거 없는 경우 추가해야 됨 (모든 청크 다 가짐)
+
+        #만약에 3번꺼를 원하는데 지금 청크 리스트에 3개가 있네? 그럼 4번째 청크를 받아야겠지?
+        want_index = len(update_chunks_list[random_client-1])
+
+        msg = "Where_is|" + str(random_client) + "|" + str(want_index)
+        client_socket.send(msg.encode("utf-8")) #서버랑 소통
 
         # 연결할 클라이언트 ip랑 포트번호 받기
         data = client_socket.recv(1024).decode()
         print(data)
 
-        target_client_list = data.split("/")
-
+        target_client_list, want_index_recv = data.split("/")
         for peer_info in target_client_list:
             target_ip, target_port = peer_info.split("|")
             # 소켓 생성
             peer_connecting_sock.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
+            time.sleep(2)
             # 다른 클라이언트랑 연결
             peer_connecting_sock[-1].connect((target_ip, int(target_port)))
 
-            peer_msg = thread_num + "번이 파일을 요청했습니다."
-            peer_connecting_sock[-1].send(peer_msg.encode("utf-8"))
+            peer_msg = thread_num + "|" + want_index_recv # (thread_num)번이 (want_index_recv)번 청크를 요청했습니다."
+            peer_connecting_sock[-1].send(peer_msg.encode("utf-8")) # 다른 피어랑 소통
 
         for i in range(len(target_client_list)):
             # 다른 클라이언트에게 파일 받기
             # 여기도 for문으로 데이터 받고 for문 안에서 파일 업데이트 실시
-            peer_data = peer_connecting_sock[i].recv(1024).decode()
+            peer_data = peer_connecting_sock[i].recv(1024).decode() # 파일 청크
+            send_client = peer_data[0] # 청크를 보낸 친구
+
+            peer_data = peer_data[1:] # 튜플 모양새 문자열만 추출
+            
+            # 여기서가 문제 왜냐면 (1300, djf;lfj;jfdjfkajfdjaf;jdk) 이런식으로 올거야.
+            # 이 문자열을 튜플로 바꿔야하는데....
             print(peer_data)
+            update_chunks_list[int(send_client)-1].append(peer_data)
+
+            time.sleep(0.01)
 
             #연결했던 클라이언트와 연결 끊기
             peer_connecting_sock[i].close()
             print("연결 끊음")
+            time.sleep(0.01)
+        peer_connecting_sock = []
 
 
 
 
 if __name__ == "__main__":
+    
     # 서버 포트 설정
     server_host = "localhost"
     server_port = 9000
@@ -66,13 +114,25 @@ if __name__ == "__main__":
     # 서버에 연결
     client_socket.connect((server_host, server_port))
 
-    
     data = client_socket.recv(1024).decode()
-    
     print(data)
 
     #서버에게 내 아이피 주소와 포트번호를 받음
     type, my_ip, my_port, thread_num = data.split("|")
+
+    #파일 불러와서 청크 단위로 쪼개서 리스트에 저장
+    file_name = chr(int(thread_num) + 64)
+    file_path = os.path.abspath(f'.\\file\\{file_name}.file')
+
+    chunks_list = read_file_in_chunks(file_path, chunk_size=256 * 1024)
+
+    print(len(chunks_list)) #1954개? 나옴
+
+    update_chunks_list = [[], [], [], []] #내가 가진 다른 클라이언트의 청크
+
+    update_chunks_list[int(thread_num)-1] = chunks_list
+
+    #print(len(update_chunks_list[int(thread_num)-1]))
 
     #아이피 주소와 포트번호로 다른 클라이언트가 들어오는걸 대기
     peer_sock.bind((my_ip, int(my_port)))
--- server.py
+++ server.py
@@ -5,34 +5,38 @@ import threading
 import random
 
 
-def client_handler(client_socket, group, thread_num):
-    global count, client_ip, client_port
+# 청크 업데이트 되는 리스트 검사해서 4개의 클라이언트가 청크를 다 받았다면 접속 종료하라고 메시지 보내주고 클라이언트 다 종료되면 서버 종료
 
-    c_list = [1, 2, 3, 4]
+def client_handler(client_socket, group, thread_num):
+    global count, client_ip, client_port, client_chunks
 
     if count == 4:
         for i, client in enumerate(group):
-            msg = "All_Connected" + "|" + client_ip[i] + "|" + str(client_port[i]) + "|" + str(i)
+            msg = "All_Connected" + "|" + client_ip[i] + "|" + str(client_port[i]) + "|" + str(i+1)
             client.send(msg.encode("utf-8"))
 
     print(1)
 
-    
     while True:
         try:
             data = client_socket.recv(1024).decode()
             print(data)
-            if data == "Where_is":
-                while True:
-                    target_num = random.choice(c_list)
-                    if target_num != thread_num:
-                        break
-                print(str(target_num) + "번 클라이언트 선택")
+            type, random_client, want_index = data.split("|") #원하는 클라이언트의 청크
+            if type == "Where_is": # 원하는 청크 갖고 있는 애 랜덤으로 고르자
+                c_list = []
+                for i in range(4): #모든 클라이언트가 가진 청크를 체크
+                    if len(client_chunks[i][int(random_client)-1]) > int(want_index): 
+                        #각 클라이언트가 원하는 클라이언트의 청크? 리스트 길이를 체크.
+                        #만약 4번째 청크(인덱스 3)를 갖고싶다 하면 그 리스트 길이가 4이상이어야 함.
+                        c_list.append(i+1)
+
+                choose_client = random.choice(c_list)
+                print(str(choose_client) + "번 클라이언트 선택")
 
                 #이 부분은 나중에 청크 가진 클라이언트 전부 보내줘야 하기 때문에 바꿔야함. 반복문으로 메시지에 추가
-                msg = client_ip[target_num-1] + "|" + str(client_port[target_num-1]) + "/"
+                msg = client_ip[choose_client-1] + "|" + str(client_port[choose_client-1]) + "/" + want_index
                 client_socket.send(msg.encode("utf-8"))
-                
+
         except:
             pass
 
@@ -47,6 +51,17 @@ if __name__ == '__main__':
     server_sock.listen(5)  # 소켓 연결, 여기서 파라미터는 접속수를 의미
 
     group, client_ip, client_port = [], [], []
+    
+    # 클라이언트가 들어오기 전에 해도 되는건지는 모르겠지만 일단 해놓음 (각 클라이언트가 가진 청크 초기)
+    client_chunks = [[[] for _ in range(4)] for _ in range(4)]
+
+    for i in range(4):
+        client_chunks[i][i] = list(range(1954))
+
+    # for i in range(4):
+    #     print(client_chunks[i]) 
+ 
+    # 각 클라이언트가 보유한 청크 목록 (계속 업데이트 되어야 함)
     count = 0
     
 
@@ -72,3 +87,5 @@ if __name__ == '__main__':
             
         except:
             pass
+
+
