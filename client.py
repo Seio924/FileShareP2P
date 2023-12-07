de3cd8ef9661e650d91cf60e9b6ea423cd9f2420
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2023-12-07 20:36:13 +0900

    system_clock

--- client.py
+++ client.py
@@ -1,105 +1,157 @@
+# -*- coding: utf-8 -*-
+
 import socket
 import threading
-import random
 import time
 import os
+import hashlib
 
-# 청크 단위로 파일 쪼개기 (완)
 
 #파일을 청크 단위로 나눠 리스트로 만들기 (청크 인덱스, 청크내용)
 def read_file_in_chunks(file_path, chunk_size=256 * 1024):
     with open(file_path, 'rb') as file:
-        index = 0
         chunks_list = []
         while True:
             chunk = file.read(chunk_size)
             if not chunk:
                 break
-            chunks_list.append((index, chunk))
-            index += 1
+            chunks_list.append(chunk)
         return chunks_list
-    
 
-def receive_messages(peer_connection, thread_num): # 여기서 해당 클라이언트에게 청크 줘
-    global chunks_list
-    try:
-        data = peer_connection.recv(1024).decode()
-        want_client, want_index_recv = data.split("|") # 내 것의 청크를 원하는 클라이언트, 원하는 청크인덱스
-        print(data)
-    except Exception as e:
-        print(f"Error receiving data: {e}")
 
+# 문자열 또는 바이트열을 MD5 해시로 변환하는 함수 (청크를 합쳐서 해시값 확인)
+def calculate_md5(data):
+    if isinstance(data, str):
+        data = data.encode('utf-8')  # 문자열을 바이트열로 변환
+
+    md5_hash = hashlib.md5()
+    md5_hash.update(data)
+    return md5_hash.hexdigest()
+
+
+def receive_messages(peer_connection): # 여기서 해당 클라이언트에게 청크 줘
+    global chunks_list, update_chunks_list, result_time
+
+    data = peer_connection.recv(256 * 1024).decode('utf-8')
+    want_file_recv, want_chunk_recv = data.split("|")
+    print(3)
     # 파일 보내주기
-    # 몇번 클라이언트의 청크인지 보내야함. 내가 몇번인지 말고 (수정)
-    print(thread_num + "번이 파일을 보냈습니다.")
-    msg = thread_num + chunks_list[int(want_index_recv)] # 친구가 원하는 청크 보내준다.
-    peer_connection.send(msg.encode("utf-8"))
+    # 클라이언트가 가지고 있는 청크 파일의 해당 경로를 알려줌
+    # 예를 들어, 클라이언트1이 가지고 있는 3번 파일의 청크는 C파일의 청크 값임을 알려주는 것임
+    start = time.time()
+
+    send_chunk = update_chunks_list[int(want_file_recv)][int(want_chunk_recv)]
+    peer_connection.sendall(want_file_recv.encode('utf-8'))
+    peer_connection.sendall(send_chunk)
+    recv_time = time.time()
+    for_time = (recv_time-start) * 1000.0
+    result_time += for_time
+    client_file.write("{:.2f} [client {}] {}번 째 청크 전달\n".format(result_time, thread_num, want_chunk_recv))
     
     
 
-def peer_handler(client_socket, thread_num):
-    global update_chunks_list
+def peer_handler(client_socket):
+    global update_chunks_list, original_file_md5, client_port, thread_num, result_time
     peer_connecting_sock = []
 
     while True:
-        #파일 나누고 자신한테 없는 파일들 정보 서버에게 물어보기
-        #랜덤으로 클라이언트 고른다. 
-        #근데 여기서 내가 나를 고를수 있잖아 그럼 다시 골라야겠지
-        #그리고 순서대로 청크를 달라고 할거기때문에 길이를 가지고 원하는 청크 정한다.
-        while True:
-            random_client = random.randint(1, 4) 
-            if random_client != thread_num or len(update_chunks_list[random_client-1]) != 1954: 
-                # 내 스레드 번호랑 다르다면 멈춰 또는 내가 고른 랜덤 클라이언트로부터 청크를 다 받았다면 다시 뽑아야지
-                break
-        
-        # 나중에 청크 더 받고 싶은거 없는 경우 추가해야 됨 (모든 청크 다 가짐)
+        update_msg = client_socket.recv(1024).decode()
 
-        #만약에 3번꺼를 원하는데 지금 청크 리스트에 3개가 있네? 그럼 4번째 청크를 받아야겠지?
-        want_index = len(update_chunks_list[random_client-1])
-
-        msg = "Where_is|" + str(random_client) + "|" + str(want_index)
-        client_socket.send(msg.encode("utf-8")) #서버랑 소통
-
-        # 연결할 클라이언트 ip랑 포트번호 받기
-        data = client_socket.recv(1024).decode()
-        print(data)
-
-        target_client_list, want_index_recv = data.split("/")
-        for peer_info in target_client_list:
-            target_ip, target_port = peer_info.split("|")
-            # 소켓 생성
-            peer_connecting_sock.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-
-            time.sleep(2)
-            # 다른 클라이언트랑 연결
-            peer_connecting_sock[-1].connect((target_ip, int(target_port)))
-
-            peer_msg = thread_num + "|" + want_index_recv # (thread_num)번이 (want_index_recv)번 청크를 요청했습니다."
-            peer_connecting_sock[-1].send(peer_msg.encode("utf-8")) # 다른 피어랑 소통
-
-        for i in range(len(target_client_list)):
-            # 다른 클라이언트에게 파일 받기
-            # 여기도 for문으로 데이터 받고 for문 안에서 파일 업데이트 실시
-            peer_data = peer_connecting_sock[i].recv(1024).decode() # 파일 청크
-            send_client = peer_data[0] # 청크를 보낸 친구
-
-            peer_data = peer_data[1:] # 튜플 모양새 문자열만 추출
+        if update_msg == "Update_Complete":            
+            #파일 나누고 자신한테 없는 파일들 정보 서버에게 물어보기
             
-            # 여기서가 문제 왜냐면 (1300, djf;lfj;jfdjfkajfdjaf;jdk) 이런식으로 올거야.
-            # 이 문자열을 튜플로 바꿔야하는데....
-            print(peer_data)
-            update_chunks_list[int(send_client)-1].append(peer_data)
+            msg = "Where_is?"
+            file_compelete = 0
+            # 4개의 청크 리스트 중에서 다 안채워진 리스트
+            for i in range(4): 
+                need_chunk = len(update_chunks_list[i])
+                if need_chunk < 1954:
+                    msg += "/" + str(i) + "|" + str(need_chunk)
+                else:
+                    file_compelete += 1
 
-            time.sleep(0.01)
+            if file_compelete == 4:
+                print("업데이트 완료")
+                i = 0
+                for chunks_list in update_chunks_list:
+                    result_content = b''.join(chunk for chunk in chunks_list)
+                    client_hash = calculate_md5(result_content)
+                    chunks_list[i] = client_hash
+                    print(chunks_list[i])
+                    i += 1
+                if i != 4:
+                    print("해시값 오류")
+                break
+            
+            client_socket.sendall(msg.encode("utf-8")) #서버랑 소통
 
-            #연결했던 클라이언트와 연결 끊기
-            peer_connecting_sock[i].close()
-            print("연결 끊음")
-            time.sleep(0.01)
-        peer_connecting_sock = []
+            # 연결할 클라이언트 ip랑 포트번호 받기
+            data = client_socket.recv(1024).decode()
 
+            target_client_list = data.split("/")
 
+            target_client_list.pop(0)
 
+            for peer_info in target_client_list:
+                target_ip, target_port, want_file_recv, want_chunk_recv = peer_info.split("|")
+             
+                # 소켓 생성
+                peer_connecting_sock.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+                
+                make_time = time.time()
+
+                # 다른 클라이언트랑 연결
+                peer_connecting_sock[-1].connect((target_ip, client_port[int(target_port)]))
+                peer_msg = want_file_recv + "|" + want_chunk_recv
+
+                peer_connecting_sock[-1].sendall(peer_msg.encode("utf-8")) # 다른 피어랑 소통
+
+                # time time
+                send_time = time.time()
+                for_time = (send_time-make_time) * 1000.0
+                result_time += for_time
+                client_file.write("{:.2f} [client {}] {}번 파일의 {}번 째 청크 요청.\n".format(result_time, thread_num, want_file_recv, want_chunk_recv))
+                
+            client_file.write("\n")
+
+            for i in range(len(target_client_list)):
+
+                # 다른 클라이언트에게 파일 받기
+                # 여기도 for문으로 데이터 받고 for문 안에서 파일 업데이트 실시
+                peer_file_num = peer_connecting_sock[i].recv(1).decode('utf-8') # 쓰레드 번호 청크
+                peer_data = peer_connecting_sock[i].recv(256*1024) # 쓰레드 번호 청크
+
+                # 여기서 (1300, djf;lfj;jfdjfkajfdjaf;jdk) 이런식으로 옴
+                # 해당 청크리스트의 클라이언트 인덱스에 차례대로 청크값만 저장
+                # 예를 들어, 클라이언트1의 128번째 청크 > update_chunks_list[0][127]
+                update_chunks_list[int(peer_file_num)].append(peer_data)
+
+                # time time
+                update_time = time.time()
+                for_time = (update_time-send_time) * 1000.0
+                result_time += for_time
+
+                client_file.write("{:.2f} [client {}] 청크 업데이트\n".format(result_time, thread_num))
+
+                #연결했던 클라이언트와 연결 끊기
+                peer_connecting_sock[i].close()
+
+                print("청크 하나 업데이트")
+            peer_connecting_sock = []
+            
+            msg = "Update_chunk_list?"
+
+            for i in range(4):
+                msg += "/" + str(i) + "|" + str(len(update_chunks_list[i]))
+                print(str(i) + " 길이 : " + str(len(update_chunks_list[i])))
+                client_file.write("{:.2f} [client {}] {} 번 파일 : {}/1954 \n".format(result_time, thread_num, str(i), str(len(update_chunks_list[i]))))
+            client_file.write("\n")
+            client_socket.sendall(msg.encode("utf-8")) #서버랑 소통
+
+    
+    client_socket.close()
+    client_file.write("서버 연결 종료\n")
+    client_file.close()
 
 if __name__ == "__main__":
     
@@ -107,46 +159,60 @@ if __name__ == "__main__":
     server_host = "localhost"
     server_port = 9000
 
+    client_port = [11111, 22222, 33333, 44444]
+
     # 소켓 생성
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     peer_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # time time
+    result_time = 0.0
     
     # 서버에 연결
     client_socket.connect((server_host, server_port))
 
     data = client_socket.recv(1024).decode()
-    print(data)
-
+    
     #서버에게 내 아이피 주소와 포트번호를 받음
-    type, my_ip, my_port, thread_num = data.split("|")
+    type_name, my_ip, thread_num = data.split("|")
 
+    file_name = "client" + thread_num + "_log.txt"     # client log 파일 생성
+    client_file = open(file_name, "w", encoding="UTF-8")
+    client_file.write("{:.2f} [client {}] 서버 연결 완료\n".format(result_time, thread_num))
+
+    
     #파일 불러와서 청크 단위로 쪼개서 리스트에 저장
     file_name = chr(int(thread_num) + 64)
     file_path = os.path.abspath(f'.\\file\\{file_name}.file')
 
     chunks_list = read_file_in_chunks(file_path, chunk_size=256 * 1024)
+    client_file.write("{:.2f} [client {}] 청크를 제공합니다.\n".format(result_time, thread_num))
 
-    print(len(chunks_list)) #1954개? 나옴
+    update_chunks_list = [[], [], [], []] #내가 가진 다른 클라이언트 청크
 
-    update_chunks_list = [[], [], [], []] #내가 가진 다른 클라이언트의 청크
+    update_chunks_list[int(thread_num)-1] = chunks_list #타입은 바이트
+    print(0)
 
-    update_chunks_list[int(thread_num)-1] = chunks_list
+    msg = "Update_chunk_list?"
+    for i in range(4):
+        msg += "/" + str(i) + "|" + str(len(update_chunks_list[i]))
 
-    #print(len(update_chunks_list[int(thread_num)-1]))
-
+    client_socket.send(msg.encode("utf-8")) #서버랑 소통
+    print(1)
     #아이피 주소와 포트번호로 다른 클라이언트가 들어오는걸 대기
-    peer_sock.bind((my_ip, int(my_port)))
+
+    peer_sock.bind((my_ip, client_port[int(thread_num)-1]))
     peer_sock.listen(4)
 
 
-    thread_main = threading.Thread(target=peer_handler, args=(client_socket, thread_num))
+    thread_main = threading.Thread(target=peer_handler, args=(client_socket,))
     thread_main.start()
-
+    print(2)
     while True:
         # Accept connection from a client
         peer_connection, peer_address = peer_sock.accept()
 
         # 스레드를 생성하여 receive_messages 함수 실행
-        thread = threading.Thread(target=receive_messages, args=(peer_connection, thread_num))
+        thread = threading.Thread(target=receive_messages, args=(peer_connection,))
         thread.start()
         
--- server.py
+++ server.py
@@ -3,67 +3,136 @@
 import socket
 import threading
 import random
+import time
+import math
 
+# 스레딩 동기화를 위한 Lock 객체 생성
+lock = threading.Lock()
 
 # 청크 업데이트 되는 리스트 검사해서 4개의 클라이언트가 청크를 다 받았다면 접속 종료하라고 메시지 보내주고 클라이언트 다 종료되면 서버 종료
 
 def client_handler(client_socket, group, thread_num):
-    global count, client_ip, client_port, client_chunks
+    global count, client_ip, client_port, client_chunks, update_client_list, result_time, start
 
     if count == 4:
         for i, client in enumerate(group):
-            msg = "All_Connected" + "|" + client_ip[i] + "|" + str(client_port[i]) + "|" + str(i+1)
+            msg = "All_Connected|" + client_ip[i] + "|" + str(i+1)
             client.send(msg.encode("utf-8"))
-
-    print(1)
+    
 
     while True:
         try:
             data = client_socket.recv(1024).decode()
-            print(data)
-            type, random_client, want_index = data.split("|") #원하는 클라이언트의 청크
-            if type == "Where_is": # 원하는 청크 갖고 있는 애 랜덤으로 고르자
-                c_list = []
-                for i in range(4): #모든 클라이언트가 가진 청크를 체크
-                    if len(client_chunks[i][int(random_client)-1]) > int(want_index): 
-                        #각 클라이언트가 원하는 클라이언트의 청크? 리스트 길이를 체크.
-                        #만약 4번째 청크(인덱스 3)를 갖고싶다 하면 그 리스트 길이가 4이상이어야 함.
-                        c_list.append(i+1)
 
-                choose_client = random.choice(c_list)
-                print(str(choose_client) + "번 클라이언트 선택")
+            data_split = data.split("?")
+            #Where_is?메시지
+            #Update_chunk_list?메시지
 
-                #이 부분은 나중에 청크 가진 클라이언트 전부 보내줘야 하기 때문에 바꿔야함. 반복문으로 메시지에 추가
-                msg = client_ip[choose_client-1] + "|" + str(client_port[choose_client-1]) + "/" + want_index
-                client_socket.send(msg.encode("utf-8"))
+            type_name = data_split.pop(0)
+
+            # Lock을 획득하여 공유 자원에 대한 동기화 보장
+            with lock:
+            
+                if type_name == "Update_chunk_list":
+                    update_time = time.time()
+                    for_time = (update_time-start) * 1000.0
+                    result_time += for_time
+                    print(str(thread_num) + "번 클라이언트의 파일 청크 업데이트")
+                    server_file.write("{:.2f} [server] '클라이언트 {}'의 파일 청크 업데이트 \n".format(result_time, thread_num))
+                    update_client_list.remove(int(thread_num))
+                    update_chunk_list = data_split[0].split("/")
+                    update_chunk_list.pop(0)
+
+                    for update_chunk in update_chunk_list:
+                        chunk_list_num, chunk_list_len = update_chunk.split("|")
+                        client_chunks[thread_num-1][int(chunk_list_num)] = int(chunk_list_len)
+                        print(chunk_list_num + "번 파일 길이 : " + chunk_list_len)
+                        server_file.write("{:.2f} [server] '클라이언트 {}'의 {}번 파일 : {}/1954 \n".format(result_time, thread_num, chunk_list_num, chunk_list_len))
+                    server_file.write("\n")
+                    if len(update_client_list) == 0:
+                        print("업데이트 완료")
+                        update_client_list = [1, 2, 3, 4]
+                        update_msg = "Update_Complete"
+                        for c_all_send in group:
+                            c_all_send.send(update_msg.encode("utf-8"))
+                        break
+
+
+                elif type_name == "Where_is": # 원하는 청크 갖고 있는 애 랜덤으로 고르자
+                    need_chunk_list = data_split[0].split("/")
+                    need_chunk_list.pop(0)
+
+
+                    target_clients_list = []
+                    target_file_num_list = []
+                    target_chunk_num_list = [] 
+                    
+                    for need_chunk in need_chunk_list:
+                        file_num, chunk_num = need_chunk.split("|")
+                        target_client = 0
+                        target_able = []
+
+                        #어떤 파일의 어떤 청크가 필요한지
+                        #더 가까운 곳에 있는 피어 고르는 거 구현
+                        for client in range(4):
+                            if client_chunks[client][int(file_num)] > int(chunk_num):
+                                target_able.append(client)
+
+                        print("파일 넘버: " + file_num)
+                        
+                        for t in target_able:
+                            if client_ip[t] == client_ip[thread_num-1]:
+                                target_client = t
+                                break
+                        
+                        if target_client == 0:
+                            target_client = random.choice(target_able)
+                        
+                        recv_time = time.time()
+                        for_time = (recv_time-update_time) * 1000.0
+                        result_time += for_time
+                        server_file.write("{:.2f} [server] '클라이언트 {}'(이)가 {}번 파일 요청 (선택된 클라이언트 : '{}')\n".format(result_time, str(thread_num), file_num, str(target_client+1)))
+                        target_clients_list.append(target_client)
+                        target_file_num_list.append(file_num)
+                        target_chunk_num_list.append(chunk_num)
+
+                        #time.sleep(0.3)
+
+                    server_file.write("\n")
+                    msg = ""
+                    for target_client, target_file_num, chunk_num in zip(target_clients_list, target_file_num_list, target_chunk_num_list):
+                        msg += "/" + client_ip[target_client] + "|" + str(target_client) + "|" + target_file_num + "|" + chunk_num
+                    
+                    client_socket.send(msg.encode("utf-8"))
 
         except:
             pass
-
+    server_sock.close()
+    server_file.write("서버 종료")
+    server_file.close()
     
 
 if __name__ == '__main__':
     
     HOST = "0.0.0.0"  # 수신 받을 모든 IP를 의미
     PORT = 9000  # 수신받을 Port
+    result_time = 0.0
+
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP Socket
     server_sock.bind((HOST, PORT))  # 소켓에 수신받을 IP주소와 PORT를 설정
-    server_sock.listen(5)  # 소켓 연결, 여기서 파라미터는 접속수를 의미
+    server_sock.listen(4)  # 소켓 연결, 여기서 파라미터는 접속수를 의미
+    
+    server_file = open("server_log.txt", "w", encoding="UTF-8")    # server log 파일 생성
 
     group, client_ip, client_port = [], [], []
-    
+    server_file.write("서버가 {}:{} 에서 실행\n".format(HOST, PORT))
+    start = time.time()
+    math.factorial(100000)
     # 클라이언트가 들어오기 전에 해도 되는건지는 모르겠지만 일단 해놓음 (각 클라이언트가 가진 청크 초기)
     client_chunks = [[[] for _ in range(4)] for _ in range(4)]
-
-    for i in range(4):
-        client_chunks[i][i] = list(range(1954))
-
-    # for i in range(4):
-    #     print(client_chunks[i]) 
+    update_client_list = [1, 2, 3, 4]
  
-    # 각 클라이언트가 보유한 청크 목록 (계속 업데이트 되어야 함)
-    count = 0
-    
+    count = 0  # 각 클라이언트가 보유한 청크 목록 (계속 업데이트 되어야 함)    
 
     while True:
         try:
@@ -80,12 +149,10 @@ if __name__ == '__main__':
             thread_num = count
             
             print('Connected ' + str(addr))
-
+            server_file.write("{:.2f} [server] '클라이언트 {}'(이)가 서버에 접속\n".format(result_time, thread_num))
             thread = threading.Thread(target=client_handler, args=(conn, group, thread_num))
             thread.start()
 
             
         except:
             pass
-
-
